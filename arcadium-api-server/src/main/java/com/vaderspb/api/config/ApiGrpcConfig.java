848a7be4e96271368434d88d083167ef525d5367
vadim.baranov <vader.zero.one@gmail.com>
2023-12-26 00:07:44 +0100

    API server

---
+++ arcadium-api-server/src/main/java/com/vaderspb/api/config/ApiGrpcConfig.java
@@ -0,0 +1,22 @@
+package com.vaderspb.api.config;
+
+import com.vaderspb.session.proto.SessionServiceGrpc;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGrpcConfig {
+    @Bean
+    public SessionServiceGrpc.SessionServiceBlockingStub sessionService(final ManagedChannel sessionServerChannel) {
+        return SessionServiceGrpc.newBlockingStub(sessionServerChannel);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public ManagedChannel sessionServerChannel() {
+        return Grpc.newChannelBuilder("job1.arcadium-worker:8080", InsecureChannelCredentials.create())
+                .build();
+    }
+}
