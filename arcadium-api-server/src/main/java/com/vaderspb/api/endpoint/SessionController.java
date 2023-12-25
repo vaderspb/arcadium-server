fc59f76c6ada56c6f2f830a9d41033c579e2eb8e
vadim.baranov <vader.zero.one@gmail.com>
2023-12-25 23:23:56 +0100

    API server

--- arcadium-api-server/src/main/java/com/vaderspb/api/endpoint/SessionController.java
+++ arcadium-api-server/src/main/java/com/vaderspb/api/endpoint/SessionController.java
@@ -1,4 +1,15 @@
 package com.vaderspb.api.endpoint;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SessionController {
+    @PostMapping("/create_session")
+    public CreateSessionResponse createSession() {
+        return new CreateSessionResponse("sessionId");
+    }
+
+    public record CreateSessionResponse(String id){
+    }
 }
