42e44187f913f37d3154101bb3cdb5fa9a4059c5
vadim.baranov <vader.zero.one@gmail.com>
2023-12-26 19:17:50 +0100

    API server

--- arcadium-session-server/src/main/java/com/vaderspb/session/config/SessionServiceConfig.java
+++ arcadium-session-server/src/main/java/com/vaderspb/session/config/SessionServiceConfig.java
@@ -4,14 +4,22 @@ import com.vaderspb.session.proto.SessionServiceGrpc;
 import com.vaderspb.session.service.SessionServiceImpl;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class SessionServiceConfig {
     @Bean
-    public SessionServiceGrpc.SessionServiceImplBase sessionService(final KubernetesClient kubernetesClient) {
-        return new SessionServiceImpl(kubernetesClient);
+    public SessionServiceGrpc.SessionServiceImplBase sessionService(
+            final KubernetesClient kubernetesClient,
+            @Value("classpath:/k8s/worker.yaml")
+            final Resource config) throws IOException {
+        return new SessionServiceImpl(kubernetesClient, config.getContentAsString(StandardCharsets.UTF_8));
     }
 
     @Bean
--- arcadium-session-server/src/main/java/com/vaderspb/session/service/SessionServiceImpl.java
+++ arcadium-session-server/src/main/java/com/vaderspb/session/service/SessionServiceImpl.java
@@ -6,21 +6,29 @@ import com.vaderspb.session.proto.SessionServiceGrpc;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.grpc.stub.StreamObserver;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
 public class SessionServiceImpl extends SessionServiceGrpc.SessionServiceImplBase {
     private final KubernetesClient kubernetesClient;
+    private final String workerConfig;
 
-    public SessionServiceImpl(final KubernetesClient kubernetesClient) {
+    public SessionServiceImpl(final KubernetesClient kubernetesClient,
+                              final String workerConfig) {
         this.kubernetesClient = kubernetesClient;
+        this.workerConfig = workerConfig;
     }
 
     @Override
     public void createSession(final CreateSessionRequest request,
                               final StreamObserver<CreateSessionResponse> responseObserver) {
-        kubernetesClient.load(SessionServiceImpl.class.getResourceAsStream("/test-resource-list.yaml"))
-                .inNamespace("default")
-                .create();
+        try {
+            kubernetesClient.load(new ByteArrayInputStream(workerConfig.getBytes(StandardCharsets.UTF_8))).create();
 
-        responseObserver.onNext(CreateSessionResponse.newBuilder().setId("sessionId").build());
+            responseObserver.onNext(CreateSessionResponse.newBuilder().setId("sessionId").build());
+        } catch (final Throwable e) {
+            responseObserver.onError(e);
+        }
         responseObserver.onCompleted();
     }
 }
