a22009afe9d2ff66dbdde521fd3f53cded38c67b
vadim.baranov <vader.zero.one@gmail.com>
2023-12-26 01:39:50 +0100

    API server

--- arcadium-session-server/src/main/java/com/vaderspb/session/config/SessionServiceConfig.java
+++ arcadium-session-server/src/main/java/com/vaderspb/session/config/SessionServiceConfig.java
@@ -2,13 +2,21 @@ package com.vaderspb.session.config;
 
 import com.vaderspb.session.proto.SessionServiceGrpc;
 import com.vaderspb.session.service.SessionServiceImpl;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SessionServiceConfig {
     @Bean
-    public SessionServiceGrpc.SessionServiceImplBase sessionService() {
-        return new SessionServiceImpl();
+    public SessionServiceGrpc.SessionServiceImplBase sessionService(final KubernetesClient kubernetesClient) {
+        return new SessionServiceImpl(kubernetesClient);
+    }
+
+    @Bean
+    public KubernetesClient kubernetesClient() {
+        return new KubernetesClientBuilder()
+                .build();
     }
 }
--- arcadium-session-server/src/main/java/com/vaderspb/session/service/SessionServiceImpl.java
+++ arcadium-session-server/src/main/java/com/vaderspb/session/service/SessionServiceImpl.java
@@ -3,12 +3,23 @@ package com.vaderspb.session.service;
 import com.vaderspb.session.proto.CreateSessionRequest;
 import com.vaderspb.session.proto.CreateSessionResponse;
 import com.vaderspb.session.proto.SessionServiceGrpc;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.grpc.stub.StreamObserver;
 
 public class SessionServiceImpl extends SessionServiceGrpc.SessionServiceImplBase {
+    private final KubernetesClient kubernetesClient;
+
+    public SessionServiceImpl(final KubernetesClient kubernetesClient) {
+        this.kubernetesClient = kubernetesClient;
+    }
+
     @Override
     public void createSession(final CreateSessionRequest request,
                               final StreamObserver<CreateSessionResponse> responseObserver) {
+        kubernetesClient.load(SessionServiceImpl.class.getResourceAsStream("/test-resource-list.yaml"))
+                .inNamespace("default")
+                .create();
+
         responseObserver.onNext(CreateSessionResponse.newBuilder().setId("sessionId").build());
         responseObserver.onCompleted();
     }
